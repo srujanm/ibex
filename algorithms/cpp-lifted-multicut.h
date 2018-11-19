1b153b4814c3e2940cd06699ec39f68f2d14ffb7
Srujan Meesala <srujanm@gmail.com>
2018-11-19 12:45:42 -0500

    deleted all files inessential for skeletonization

--- algorithms/cpp-lifted-multicut.h
+++
@@ -1,1 +0,0 @@
-unsigned char *CppLiftedMulticut(unsigned long nvertices, unsigned long nedges, unsigned long *vertex_ones, unsigned long *vertex_twos, double *lifted_weights, double beta, unsigned int heuristic);
